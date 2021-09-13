0266940731c6ca046b8baa1850ce08ba0330fa68
JosueEPN <65979995+JosueEPN@users.noreply.github.com>
2021-09-13 16:36:57 -0500

    Juego de Memoria

---
+++ src/import.js
@@ -0,0 +1,59 @@
+import src1 from "./images/sub25/3c.jpg";
+import src2 from "./images/sub25/8c.jpg";
+import src3 from "./images/sub25/2c.jpg";
+import src4 from "./images/sub25/5c.jpg";
+import src5 from "./images/sub25/6c.jpg";
+import src6 from "./images/sub25/7c.jpg";
+
+const images = [
+  {
+    src: src1,
+    player: "imagen1",
+  },
+  {
+    src: src1,
+    player: "imagen1",
+  },
+  {
+    src: src2,
+    player: "imagen2",
+  },
+  {
+    src: src2,
+    player: "imagen2",
+  },
+  {
+    src: src3,
+    player: "imagen3",
+  },
+  {
+    src: src3,
+    player: "imagen3",
+  },
+  {
+    src: src4,
+    player: "imagen4",
+  },
+  {
+    src: src4,
+    player: "imagen4",
+  },
+  {
+    src: src5,
+    player: "imagen5",
+  },
+  {
+    src: src5,
+    player: "imagen5",
+  },
+  {
+    src: src6,
+    player: "imagen6",
+  },
+  {
+    src: src6,
+    player: "imagen6",
+  },
+];
+
+export { images };
